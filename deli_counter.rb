47aebd6868a54d57253605b264e93056654bc4c6
Rwanda S <rwanda.ace@gmail.com>
2020-03-09 15:10:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,13 @@
 
 katz_deli = [] # daily deli counter
 
-def line(arrayKatz_deli)
+def line(katz_deli)
   
-  if arrayKatz_deli.size > 0 
-    puts "Your place in line is #{arrayKatz_deli.size}"
+  if katz_deli.size > 0 
+    
+    
+    
+    puts "The line is currently:  #{arrayKatz_deli.size}"
   else
     puts "The line is currently empty."
   
@@ -13,6 +16,6 @@ end
 
 def take_a_number(katz_deli, your_name)
   
-  puts "#{your_name} #{katz_deli.size + 1}"
+  puts "Welcome, #{your_name}. You are number #{katz_deli.size + 1} in line."
   
 end
