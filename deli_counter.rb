1c0bd3b50304fc51fb6fc5a8207feb64909bbc19
Rwanda S <rwanda.ace@gmail.com>
2020-03-09 15:32:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,10 @@ end
 
 def now_serving(katz_deli)
   
-  
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be serverd!"
+  else
+    puts "Currently serving #{katz_deli[0]}"
+  end
   
 end
